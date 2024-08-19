2761aa85fa2f949f6722b4a4d18699383be869bf
Kaku Lakshmi Narasimha Yadav <149588823+narasimha-github@users.noreply.github.com>
2024-08-19 14:04:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Header from './components/Header'
 import Body from './components/Body'
-import './App.css';
+import Footer from './components/Footer'
+import './App.css'
 
 function App() {
   return (
     <>
       <Header />
       <Body />
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
