69d9f5ee55ad4c9efb2ba8f6bab8037d9edbc3ba
Kaku Lakshmi Narasimha Yadav <149588823+narasimha-github@users.noreply.github.com>
2024-08-19 14:03:39 +0530

    Update index.js

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -4,7 +4,7 @@ import './index.css'
 
 const Products = props => {
     const {eachItem} = props
-    const {category, description, image, price, rating, title} = eachItem
+    const {category, image, price, rating, title} = eachItem
     const {rate, count} = rating
 
     return(
@@ -12,7 +12,6 @@ const Products = props => {
                <img src={image} alt='Each Item' className='productImage' />
                <h3 className='eachProductTitle'>{title}</h3>
                <h3 className='eachProductCategory'>{category}</h3>
-               <p className='eachProductDescription'>{description}</p>
                <h2 className='eachProductCost'>{`RS :${price}/-`}</h2>
                <div className='ratingAndStockContainer'>
                  <p className='eachProductRating'><FontAwesomeIcon icon={faStar} /> {rate}</p>
